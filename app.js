ea09d0bc63a4281fd92870ddd2e9e492a92005d4
miriam <miriamhuertahernandez2018@gmail.com>
2024-03-30 16:43:36 -0600

    udemy

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+
+        var nombre = prompt("Hola, ¿Cuál es tu nombre?");
+        var edad = prompt( "ingresa tu edad");
+
+        alert("Bienvenido" + nombre);
+
+
+
+        //Pedir la base de y altura del rectángulo
+         
+        var base = parseFloat(prompt("Por favor, ingresa la base del rectángulo:")); 
+        var altura = parseFloat(prompt("Ingresa la altura del rectángulo:"));
+
+         //calcular el área del rectángulo
+         var area = base * altura;
+
+         //Mostrar el resultado
+        alert( "El área del rectángulo es:" + area);
+
+
+        //Crear un array con números.
+     let numeros = [ 1, 2, 3, 4, 5];
+     // Sumar los valore del array accediendo a sus índices
+     let total = numeros [0] + [1] + [2] + [3] + [4];
+     //Mostrar el resultado
+     alert('La suma de los valores del array es: ${total}');
+
+
+
+     //falta ejercicio numeros  
